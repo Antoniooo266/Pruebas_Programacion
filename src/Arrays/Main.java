150ca02f1a9076498ab8c3f1d9057436928fe5da
Antonio <Antonio@LAPTOP-Q1IEL971>
2021-02-25 14:13:56 +0100

    Arrays

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,26 +1,49 @@
 package Arrays;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Ejercicio1();
+        Ejercicio3();
 
     }
-    public static void Ejercicio1(){
+
+    public static void Ejercicio1() {
         Scanner sc = new Scanner(System.in);
-        int numeros[] = new int[4];
+        int[] numeros = new int[4];
         float media = 0;
-        for (int i=0; i < numeros.length; i++){
+        for (int i = 0; i < numeros.length; i++) {
             System.out.println("Ingrese un numero: ");
             int num = sc.nextInt();
             numeros[i] = num;
             media += numeros[i];
             sc.close();
         }
-        System.out.println("La media es " + media/4);
+        System.out.println("La media es " + media / 4);
     }
-    public static void Ejercicio2(){
-        Scanner sc = new Scanner(System.in);
 
+    public static void Ejercicio2() {
+        Scanner sc = new Scanner(System.in);
+        float[] numeros = new float[5];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese un numero: ");
+            int num = sc.nextInt();
+            numeros[i] = num;
+        }
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
+
+    public static void Ejercicio3(){
+        Scanner sc = new Scanner(System.in);
+        int[] dmes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for (int i= 0;i < dmes.length; i++){
+            System.out.println("Indique el mes: ");
+            int mes = sc.nextInt();
+            dmes[i] = mes;
+            sc.close();
+        }
+        System.out.println("El mes seleccionado tiene ");
     }
 }
